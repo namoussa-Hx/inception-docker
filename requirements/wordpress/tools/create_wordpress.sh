13524cee56fe9ac5a95a709a1cb273f5eb8fdb29
Mohamed Amoussaoui <mohamedamoussaoui20@gmail.com>
2025-02-10 14:29:09 +0100

    we

--- requirements/wordpress/tools/create_wordpress.sh
+++ requirements/wordpress/tools/create_wordpress.sh
@@ -10,9 +10,18 @@ cd /var/www/html
 rm -rf *
 
 
+
 wp core download --allow-root
-wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+sed -i "s/username_here/$WP_DATABASE_USR/g" wp-config-sample.php
+sed -i "s/password_here/$WP_DATABASE_PWD/g" wp-config-sample.php
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/database_name_here/$WP_DATABASE_NAME/g" wp-config-sample.php
+
+
+cp wp-config-sample.php wp-config.php
+
+wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOSTNAME --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITL --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL  --allow-root
 wp plugin update --all --allow-root
 
 exec /usr/sbin/php-fpm7.3 -F
